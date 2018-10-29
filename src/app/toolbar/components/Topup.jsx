7f2d34970b5f2a7ba6da8ab165c252a3d542972c
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2018-10-29 18:42:36 +0200

    WIP

--- src/app/toolbar/components/Topup.jsx
+++ src/app/toolbar/components/Topup.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Modal } from 'semantic-ui-react';
 
-const Topup = () => (
+import withModal from '../../../lib/withModal';
+
+/**
+ * Button that triggers top-up dialog
+ */
+const TopupButton = ({ onClick }) => (
     <Button
+        onClick={onClick}
         content="Top-up"
         icon="arrow alternate circle down"
         color="red"
@@ -10,4 +16,36 @@ const Topup = () => (
     />
 );
 
-export default Topup;
+/**
+ * Dialog with form that allow to specify ammount for top-up
+ */
+const TopupDialog = ({ open, onOpen, onClose }) => (
+    <Modal
+        open={open}
+        onClose={onClose}
+        trigger={<TopupButton onClick={onOpen} />}
+        size="small"
+    >
+        <Modal.Header>Top-up contract balance</Modal.Header>
+        <Modal.Content>[TOP-UP FORM GOES HERE]</Modal.Content>
+        <Modal.Actions>
+            <Button
+                // onClick={handleSubmit}
+                // disabled={pristine || submitting}
+                // loading={submitting}
+                primary
+            >
+                Execute
+            </Button>
+            <Button
+                onClick={onClose}
+                icon="remove"
+                content="Cancel"
+                //disabled={submitting}
+                basic
+            />
+        </Modal.Actions>
+    </Modal>
+);
+
+export default withModal(TopupDialog);
