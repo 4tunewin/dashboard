3e3d7db639ba4872674d4bc818caecaf71112237
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-01-18 17:33:51 +0200

    Switch to rinkeby network

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,6 +1,6 @@
 export default {
     // An URL of block explorer
-    explorerUrl: 'https://kovan.etherscan.io',
+    explorerUrl: 'https://rinkeby.etherscan.io',
     graphql: {
         uri: 'graphql.4tune.win',
     },
