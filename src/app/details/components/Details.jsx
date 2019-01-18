570b023ed1a563f8232eea066748da30cce67f3c
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-01-18 23:03:30 +0200

    Remove unsuded import

--- src/app/details/components/Details.jsx
+++ src/app/details/components/Details.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Icon } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 
 import { AsyncValue, ExplorerLink, CopyButton } from '../../common';
 
