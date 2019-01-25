6cf170a8c811903af896ed08e4e0e2948b391af2
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-01-25 18:39:00 +0200

    Prepare for prod

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,6 +1,6 @@
 export default {
     // An URL of block explorer
-    explorerUrl: 'https://rinkeby.etherscan.io',
+    explorerUrl: 'https://wwww.etherscan.io',
     graphql: {
         uri: 'graphql.4tune.win',
     },
