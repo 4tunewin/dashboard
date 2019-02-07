ee6a12cdc2cc10267991600595fb5fc551f886d6
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-02-08 00:33:01 +0200

    Change graphql uri

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -2,6 +2,6 @@ export default {
     // An URL of block explorer
     explorerUrl: 'https://wwww.etherscan.io',
     graphql: {
-        uri: 'graphql.4tune.win',
+        uri: 'graphql.4tune.win:8080',
     },
 };
